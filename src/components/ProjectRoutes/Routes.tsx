a0dfbcc57177d8b5449c4a5cb1034d87d4e02447
Dmitry Kulikov <Kulikov.DV@parma.ru>
2023-06-21 12:30:14 +0300

    добавил пример с папками

--- src/components/ProjectRoutes/Routes.tsx
+++ src/components/ProjectRoutes/Routes.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import { Guest } from "../../pages/Guest";
 import { NoMatch } from "../../pages/NoMatch";
+import { User } from "../../pages/User";
 import SiteLayout from "../Layout";
 import { PROJECT_NAME } from "../../constants/main";
 import Login from "../../pages/Login";
+
 
 export const ProjectRoutes = () => {
   return (
@@ -11,6 +13,7 @@ export const ProjectRoutes = () => {
       <Route path="/" element={<SiteLayout header={<span>{PROJECT_NAME}</span>}/>}>
         <Route index element={<Login />} />
         <Route path="guest" element={<Guest />} />
+        <Route path="user" element={<User />} />
         <Route path="*" element={<NoMatch />} />
       </Route>
     </Routes>
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,3 +1,5 @@
 const API_ROOT = 'http://localhost:3000/api';
 
 export const API_GUEST = `${API_ROOT}/scope/guest`;
+
+export const API_FOLDERS = `${API_ROOT}/scope/guest/folders`;
