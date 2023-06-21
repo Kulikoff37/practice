e1fa2fed3c7adb0303eb48ab87d342f4dca9dcc0
Dmitry Kulikov <Kulikov.DV@parma.ru>
2023-06-21 10:43:52 +0300

    добавил devtools

--- src/pages/Guest/store.ts
+++ src/pages/Guest/store.ts
@@ -1,13 +1,14 @@
 import { create } from 'zustand';
 import { getUserSpace } from './api';
 import { IUserSpaceState } from '../../types/user-space.types';
+import { devtools } from 'zustand/middleware'
 
-export const useUserSpace = create<IUserSpaceState>((set) => ({
+export const useUserSpace = create<IUserSpaceState>()(devtools((set) => ({
   space: null,
   loading: false,
   error: null,
   fetchSpace: async () => {
     const response = await getUserSpace();
-    set({ space: response })
+    set({ space: response }, false, 'setScope')
   },
-}));
+})));
